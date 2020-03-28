0ca3fa279c5e1048241651c1be993c7c0a39cc17
Steve Zdancewic <stevez@cis.upenn.edu>
2020-03-28 09:07:37 -0400

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Steve Zdancewic on 3/28/20.
+//  Copyright © 2020 Steve Zdancewic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
